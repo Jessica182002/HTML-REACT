b8aec66d520c0427102b7d6dca6b0d84c696c15f
Jessica182002 <josephsalazar550@gmail.com>
2025-01-21 08:25:34 -0500

    login

---
+++ src/componentes/Login.jsx
@@ -0,0 +1,14 @@
+
+const Login = () => {
+    return(
+        <form>
+            <h1>Login</h1>
+            <label>Correo</label>
+            <input type="text" />
+            <label>Contraseña</label>
+            <input type="text" />
+            <button>iniciar sesion</button>
+        </form>
+    )
+}
+export default Login;
---
+++ src/componentes/Registro.jsx
@@ -0,0 +1,16 @@
+
+const Registro = () => {
+    return(
+        <form>
+            <h1>Registro</h1>
+            <label >Nombre</label>
+            <input type="ññ" />
+            <label>Correo</label>
+            <input type="text" />
+            <label>Contraseña</label>
+            <input type="text" />
+            <button>registrar</button>
+        </form>
+    )
+}
+export default Registro;
